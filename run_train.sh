5b7dc1f4a495f30fc360deb29c5c90ea19817739
Sondre Wold <sondrewold1@gmail.com>
2022-08-17 16:54:47 +0200

    Hyperparameter sweep

--- run_train.sh
+++ run_train.sh
@@ -1,14 +1,37 @@
 
-python train.py --model="roberta-base" --use_graph --epochs=3 --lr=3e-5 --pg
-python train.py --model="roberta-base" --use_graph --epochs=3 --lr=3e-5
-python train.py --model="roberta-base" --epochs=3 --lr=3e-5
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4 --pg
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4
+python train.py --model="roberta-base" --epochs=5 --lr=1e-4
+
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5 --pg
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5
+python train.py --model="roberta-base" --epochs=5 --lr=3e-5
 
 python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5 --pg
 python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5
 python train.py --model="roberta-base" --epochs=5 --lr=4e-5
 
-python train.py --model="roberta-base" --use_graph --epochs=3 --lr=5e-5 --pg
-python train.py --model="roberta-base" --use_graph --epochs=3 --lr=5e-5
-python train.py --model="roberta-base" --epochs=3 --lr=5e-5
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5 --pg
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5
+python train.py --model="roberta-base" --epochs=5 --lr=5e-5
+
+
+#Batch size 32
+
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4 --pg --batch_size=32
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4 --batch_size=32
+python train.py --model="roberta-base" --epochs=5 --lr=1e-4 --batch_size=32
+
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5 --pg --batch_size=32
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5 --batch_size=32
+python train.py --model="roberta-base" --epochs=5 --lr=3e-5 --batch_size=32
+
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5 --pg --batch_size=32
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5 --batch_size=32
+python train.py --model="roberta-base" --epochs=5 --lr=4e-5 --batch_size=32
+
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5 --pg --batch_size=32
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5 --batch_size=32
+python train.py --model="roberta-base" --epochs=5 --lr=5e-5 --batch_size=32
 
 
