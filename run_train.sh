a91ba248f5c8ac3e58fd6af02d084b199e5debe4
ec-sondrewo <ec-sondrewo@login-1.fox.ad.fp.educloud.no>
2022-08-17 16:49:06 +0200

    add run train file

---
+++ run_train.sh
@@ -0,0 +1,14 @@
+
+python train.py --model="roberta-base" --use_graph --epochs=3 --lr=3e-5 --pg
+python train.py --model="roberta-base" --use_graph --epochs=3 --lr=3e-5
+python train.py --model="roberta-base" --epochs=3 --lr=3e-5
+
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5 --pg
+python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5
+python train.py --model="roberta-base" --epochs=5 --lr=4e-5
+
+python train.py --model="roberta-base" --use_graph --epochs=3 --lr=5e-5 --pg
+python train.py --model="roberta-base" --use_graph --epochs=3 --lr=5e-5
+python train.py --model="roberta-base" --epochs=3 --lr=5e-5
+
+
