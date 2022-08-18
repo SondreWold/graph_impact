9dcf2eeaf3cc6daf2fad1b24ea41e55dbb335cea
ec-sondrewo <ec-sondrewo@login-3.fox.ad.fp.educloud.no>
2022-08-18 15:55:07 +0200

    test file

---
+++ run_test_all.sh
@@ -0,0 +1,12 @@
+
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs --rg
+python train.py --model="bert-base-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs --rg
+
