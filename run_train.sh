92990bef08c0679010f2e446bee86dfe91469916
Sondre Wold <sondrewold1@gmail.com>
2022-08-18 08:04:02 +0200

    sweep, different seed

--- run_train.sh
+++ run_train.sh
@@ -1,37 +1,33 @@
 
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4 --pg
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4
-python train.py --model="roberta-base" --epochs=5 --lr=1e-4
 
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5 --pg
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5
-python train.py --model="roberta-base" --epochs=5 --lr=3e-5
 
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5 --pg
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5
-python train.py --model="roberta-base" --epochs=5 --lr=4e-5
+#Batch size 16
 
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5 --pg
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5
-python train.py --model="roberta-base" --epochs=5 --lr=5e-5
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=3e-5 --pg 
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=3e-5
+python train.py --seed=1234 --model="roberta-base" --epochs=4 --lr=3e-5
+
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=4e-5 --pg
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=4e-5
+python train.py --seed=1234 --model="roberta-base" --epochs=4 --lr=4e-5
+
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=5e-5 --pg
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=5e-5
+python train.py --seed=1234 --model="roberta-base" --epochs=4 --lr=5e-5
 
 
 #Batch size 32
 
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4 --pg --batch_size=32
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=1e-4 --batch_size=32
-python train.py --model="roberta-base" --epochs=5 --lr=1e-4 --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=3e-5 --pg --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=3e-5 --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --epochs=4 --lr=3e-5 --batch_size=32
 
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5 --pg --batch_size=32
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=3e-5 --batch_size=32
-python train.py --model="roberta-base" --epochs=5 --lr=3e-5 --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=4e-5 --pg --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=4e-5 --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --epochs=4 --lr=4e-5 --batch_size=32
 
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5 --pg --batch_size=32
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=4e-5 --batch_size=32
-python train.py --model="roberta-base" --epochs=5 --lr=4e-5 --batch_size=32
-
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5 --pg --batch_size=32
-python train.py --model="roberta-base" --use_graph --epochs=5 --lr=5e-5 --batch_size=32
-python train.py --model="roberta-base" --epochs=5 --lr=5e-5 --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=5e-5 --pg --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --use_graph --epochs=4 --lr=5e-5 --batch_size=32
+python train.py --seed=1234 --model="roberta-base" --epochs=4 --lr=5e-5 --batch_size=32
 
 
