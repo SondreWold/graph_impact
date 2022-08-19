95b636086d7fba39e93aef3eb381010ea28424fa
Sondre Wold <sondrewold1@gmail.com>
2022-08-19 16:18:12 +0200

    tests for large models

--- run_test_all.sh
+++ run_test_all.sh
@@ -1,45 +1,88 @@
 
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs --rg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs --rg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs --rg
 
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs
 
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs --pg
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs --pg
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs --pg
 
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test
-python train.py --model="roberta-base" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test
+python train.py --model="bert-large-uncased" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test
 
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs --rg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs --rg
+
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs
+
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test --use_graphs --pg
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test --use_graphs --pg
+
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=667 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=321 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=412 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=233 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=512 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1252 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=421 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=1245 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=4 --test
+python train.py --model="roberta-large" --epochs=10 --batch_size=8 --lr=1e-5 --seed=3 --test
