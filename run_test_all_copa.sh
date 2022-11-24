04b7db91cae445e39288b7f7f3fb63fdd452df13
Sondre Wold <sondrewold1@gmail.com>
2022-11-24 12:10:51 +0100

    Copa test file

---
+++ run_test_all_copa.sh
@@ -0,0 +1,44 @@
+
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=667
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=321
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=412
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=233
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=512
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1252
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=421
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1245
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=4
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=3
+
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=667 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=321 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=412 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=233 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=512 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1252 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=421 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1245 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=4 --use_graphs
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=3 --use_graphs
+
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=667 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=321 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=412 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=233 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=512 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1252 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=421 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1245 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=4 --use_graphs --pg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=3 --use_graphs --pg
+
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=667 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=321 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=412 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=233 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=512 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1252 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=421 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=1245 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=4 --use_graphs --rg
+python train.py --task="copa" --model="bert-base-uncased" --epochs=5 --batch_size=16 --lr=3e-5 --seed=3 --use_graphs --rg
