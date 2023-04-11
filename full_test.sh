2738677c332078f2309e218f4fc1219cfe0dd320
Sondre Wold <sondrewold1@gmail.com>
2023-04-11 08:53:52 +0200

    reduce n of runs

--- full_test.sh
+++ full_test.sh
@@ -1,21 +1,18 @@
 
-for seed in 42 1443 1790 1234 5432 654 3 222 90 12
+for seed in 9 117269 76230 41480 60050
 do
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=8 --lr=4e-5 --weight_decay=0.1 --test
-    
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --el
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --rg
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --pgg
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --pgl
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=8 --lr=4e-5 --weight_decay=0.1 --test
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --el
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --rg
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --pgg
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-base-uncased" --epochs=10 --task="expla" --batch_size=16 --use_graphs --lr=3e-5 --weight_decay=0.01 --test --pgl
 
-
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=16 --lr=4e-6 --weight_decay=0.1 --test
-    
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --el
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --rg
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --pgg
-    python3 train.py --seed=$seed -dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --pl
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=16 --lr=4e-6 --weight_decay=0.1 --test
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --el
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --rg
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --pgg
+    python3 train.py --seed=$seed --dropout=0.3 --model="bert-large-uncased" --epochs=10 --task="copa" --batch_size=8 --use_graphs --lr=4e-5 --weight_decay=0.01 --test --pl
 
 done
